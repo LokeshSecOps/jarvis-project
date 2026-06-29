5466d107ac137d4317514ac7a09c5b751a93b932
LokeshSecOps <Lokeshsharma02110@gmail.com>
2026-06-29 21:31:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify, render_template, session
 from groq import Groq
 from dotenv import load_dotenv
 
-load_dotenv() 
+load_dotenv()  
 
 app = Flask(__name__)
 
